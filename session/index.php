f70d9b7d348f546e738dd21a2b3cfb7b95bb3a9d
Saran2602 <65610502+Saran2602@users.noreply.github.com>
2021-01-27 17:45:13 +0530

    Update index.php

--- session/index.php
+++ session/index.php
@@ -9,7 +9,7 @@ session_start();
 
 // Echo session variables that were set on previous page
 echo "Favorite color is " . $_SESSION["favcolor"]="blue". ".<br>";
-echo "Favorite animal is " . $_SESSION["favanimal"]="ebi" . ".";
+echo "Favorite animal is " . $_SESSION["favanimal"]="tiger" . ".";
 ?>
 
 </body>
